5fab1ee317d8f17ad651ea36ab4df7ef52eb35be
Sajjad-Hosan <s4jj4dho34n@hmail.com>
2024-07-02 15:34:16 +0600

    humanity helper

--- src/components/OurTeams/OurTeams.jsx
+++ src/components/OurTeams/OurTeams.jsx
@@ -10,14 +10,13 @@ const OurTeams = () => {
         axios.get('/teams.json')
         .then(res => setTeams(res.data))
     },[])
-    console.log(teams)
     return (
         <div className="mt-20 w-full px-4 md:px-10">
             <h1 className="text-lg md:text-3xl mb-8 text-center">Our Team</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-8">
                 <Bounce>
                 {
-                    teams.map(item => <MemberCard key={item.id}  data={item} />)
+                    teams.map(item => <MemberCard key={item?.id}  data={item} />)
                 }
                 </Bounce>
             </div>
